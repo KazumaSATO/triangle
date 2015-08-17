e5a38c1d3beaf4fbbd47e4093667f45e449d6c8f
Kazuma SATO <r.nakamura1989@gmail.com>
2015-08-17 23:15:26 +0900

    db connection

--- src/triangle/db.clj
+++ src/triangle/db.clj
@@ -1,2 +1,19 @@
-(ns triangle.db)
+(ns triangle.db
+  (:use [korma.db]
+        [korma.core]
+        [clojure.repl]))
 
+(def db (mysql {:db "triangle"
+                :host "localhost"
+                :port "3306" 
+                :user "root"
+                :password ""}))
+(defdb korma-db db)
+
+
+(defentity file)
+
+(select file)
+
+;(insert file 
+;  (values {:img_id "john" :original_file_url "doe"}))
