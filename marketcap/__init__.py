f8e6f987c2a92f6b0cf7cfeb28ca7ed1115d966b
Ryotaro Nakamura <nakamura.ryotaro.kzs@gmail.com>
2023-09-12 19:50:54 -0400

    fix: PyMYSQL is not thread safe.

--- marketcap/__init__.py
+++ marketcap/__init__.py
@@ -5,25 +5,28 @@ import pymysql
 app = FastAPI()
 
 
-connection = pymysql.connect(
-    host="127.0.0.1",
-    user="initial",
-    password="initial",
-    db="initial",
-    charset="utf8mb4",
-    cursorclass=pymysql.cursors.DictCursor,
-)
+def connect():
+    return pymysql.connect(
+        host="127.0.0.1",
+        user="initial",
+        password="initial",
+        db="initial",
+    )
 
 
 @app.get("/marketcaps/{company_id}")
 def read_root(company_id: int):
-    with connection.cursor() as cursor:
-        sql = """
-        select market_cap from disruptor
-        where company_company_id = %s and market_cap is not null limit 1
-        """
-        cursor.execute(sql, (company_id))
-        result = cursor.fetchone()
-        if result is not None:
-            result = result['market_cap']
-        return {'market capitalization': result}
+    connection = connect()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = """
+            select market_cap from disruptor
+            where company_company_id = %s and market_cap is not null limit 1
+            """
+            cursor.execute(sql, (company_id))
+            result = cursor.fetchone()
+            print(company_id, result)
+            if result is not None:
+                result = result[0]  # ["market_cap"]
+
+            return {"marketCapitalization": result}
